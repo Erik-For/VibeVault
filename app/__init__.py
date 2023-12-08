adbc495686b0ec552f997b3a3573062823bfc246
Erik-For <erik@forsum.se>
2023-12-08 02:30:23 +0100

    fixed mail issues

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv(".flaskenv")
 
 app = Flask(__name__)
 
 app.config.update(
     MAIL_SERVER=os.getenv('MAIL_SERVER'),
-    MAIL_PORT=587,
-    MAIL_USE_TLS=True,
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
     MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'),
     MAIL_DEFAULT_SENDER=("VibeVault", os.getenv('MAIL_USERNAME'))
